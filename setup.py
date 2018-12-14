55f6957424a9178a02d607af19796c02975ce1e2
zizonk <afifnurazizi@gmail.com>
2018-12-15 00:39:52 +0800

    initial commit

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='ohmlaws',
         version='0.1',
         description='The formula of electrical enginering ',
-        url='http://github.com/storborg/funniest',
+        url='https://github.com/zizonk/electrical-formula.git',
         author='M afif Nur Azizi',
         author_email='afifnurazizi@gmail.com',
         license='MIT',
