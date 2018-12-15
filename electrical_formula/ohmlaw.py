7ced0427bfebe18a923daf2be3afb4ec3cbd522e
zizonk <afifnurazizi@gmail.com>
2018-12-15 13:48:44 +0800

    initial commit

--- electrical_formula/ohmlaw.py
+++ electrical_formula/ohmlaw.py
@@ -1,34 +1,27 @@
-class Ohm:
+def __init__(self):
     
+    pass
+def current(self,voltage,resistance):
+    i = voltage/resistance
+    print(i)
+    return i
+def resistance_basic(self,resistivity,length,area):
+    r =resistivity*(length/area)
+    print(r)
+    return r
+def resistance(self,current,voltage):
+    r2 = current/voltage
+    print(r2)
+    return r2
 
-    def __init__(self):
-        
-        pass
-    def current(self,voltage,resistance):
-        i = voltage/resistance
-        print(i)
-        return i
-    def resistance_basic(self,resistivity,length,area):
-        r =resistivity*(length/area)
-        print(r)
-        return r
-    def resistance(self,current,voltage):
-        r2 = current/voltage
-        print(r2)
-        return r2
-    
-    def voltage(self,current,resistance):
-        v = current*resistance
-        print(v)
-        return  v
-
-class Mho(Ohm):
-    def __init__(self):
-        super(Mho,self).__init__()
-    def mho (self,resistance):
-        m = 1/resistance
-        print(m)
-        return m
+def voltage(self,current,resistance):
+    v = current*resistance
+    print(v)
+    return  v
+def mho (self,resistance):
+    m = 1/resistance
+    print(m)
+    return m
 
 
 
