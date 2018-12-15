457fd1c4908f3a818026534b81aa11c0f64e33bb
zizonk <afifnurazizi@gmail.com>
2018-12-15 14:39:21 +0800

    initial commit

--- electrical_formula/ohmlaw.py
+++ electrical_formula/ohmlaw.py
@@ -1,27 +1,33 @@
-def __init__(self):
+class Ohm:
     
-    pass
-def current(self,voltage,resistance):
-    i = voltage/resistance
-    print(i)
-    return i
-def resistance_basic(self,resistivity,length,area):
-    r =resistivity*(length/area)
-    print(r)
-    return r
-def resistance(self,current,voltage):
-    r2 = current/voltage
-    print(r2)
-    return r2
+    @staticmethod
+    def current(voltage,resistance):
+        i = voltage/resistance
+        print(i)
+        return i
+    @staticmethod
+    def resistance_basic(resistivity,length,area):
+        r =resistivity*(length/area)
+        print(r)
+        return r
+    @staticmethod
+    def resistance(current,voltage):
+        r2 = current/voltage
+        print(r2)
+        return r2
+    @staticmethod
+    def voltage(current,resistance):
+        v = current*resistance
+        print(v)
+        return  v
 
-def voltage(self,current,resistance):
-    v = current*resistance
-    print(v)
-    return  v
-def mho (self,resistance):
-    m = 1/resistance
-    print(m)
-    return m
+class Mho(Ohm):
+    def __init__(self):
+        super(Mho,self).__init__()
+    def mho (self,resistance):
+        m = 1/resistance
+        print(m)
+        return m
 
 
 
--- setup.py
+++ setup.py
@@ -7,5 +7,5 @@ setup(name='ohmlaws',
         author='M afif Nur Azizi',
         author_email='afifnurazizi@gmail.com',
         license='MIT',
-        packages=['electrical_formula'],
+        packages=['ohm_formula'],
         zip_safe=False)
