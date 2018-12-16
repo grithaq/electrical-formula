a00f5801c169b2f2650b66dcbcc94e25bc265a61
zizonk <afifnurazizi@gmail.com>
2018-12-16 22:09:16 +0800

    initial comit

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
-from setuptools import setup
+import setuptools
 
-setup(name='ohmlaws',
-        version='0.1',
-        description='The formula of electrical enginering ',
-        url='https://github.com/zizonk/electrical-formula.git',
-        author='M afif Nur Azizi',
-        author_email='afifnurazizi@gmail.com',
-        license='MIT',
-        packages=['ohm_formula'],
-        
-        dependency_links=[
-                'https://github.com/zizonk/electrical-formula'
-        ],
-        install_requires=[
-          'markdown'
-          ],
-        zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ohmlaws",
+    version="0.0.1",
+    author="M afif Nur Azizi",
+    author_email="afifnurazizi@gmail.com",
+    description="The formula of electrical enginering",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/zizonk/electrical-formula.git",
+    packages=['electrical_formula'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
